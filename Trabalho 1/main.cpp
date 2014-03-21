69cbab15256d0e03d50dd213b213d04095194e29
Matheus Steck Cardoso <mathsteck@gmail.com>
2014-03-21 17:30:44 -0300

    Recreate all the code
        * Recreate the Node class and make it accept void pointers
          to store any value (a Node or a integer, for example).
        * Create a List class with some basic operations (need to make
          remove and destroy).

--- Trabalho 1/main.cpp
+++ Trabalho 1/main.cpp
@@ -27,87 +27,94 @@
 */
 
 #include <iostream>
+#include <typeinfo>
 #include <stdlib.h>
 
 using namespace std;
 
-#define TYPE int
-#define MAX_CHILDREN 8
-
 class Node {
-    TYPE value;
-    Node **children = NULL, *father = NULL;
-    int max_children, number_of_children;
+
+    void *content;
+    Node *next, *previous;
 
     public:
+        Node(void);
 
-        Node(int, int);
-
-        /* Setters */
-
-        int set_value(TYPE new_value) {
-            if(&new_value != NULL) {
-                value = new_value;
-                return true;
-            }
-            return false;
+        void add_next(Node *node) {
+            next = node;
+        }
+        void add_previous(Node *node) {
+            previous = node;
         }
 
-        int set_node(Node *new_node) {
-            if(new_node == NULL) {
-                cout << "ERROR: Null node's adress\n";
-                return false;
-            }
-            if(number_of_children + 1 < max_children) {
-                new_node->father = this;
-                children[number_of_children] = new_node;
-                number_of_children++;
-                return true;
-            }
-            cout << "Max children's limit reached\n";
-            return false;
+        void add_content(void *new_content) {
+            content = new_content;
         }
 
-        /* Getters */
-
-        TYPE get_value(void) {
-            return value;
+        void* get_content(void) {
+            return content;
         }
 
-        TYPE get_node_value(int node_number) {
-            if(node_number < max_children && node_number >= 0) {
-                cout << node_number << " " << max_children << endl;
-                return children[node_number]->get_value();
-            }
-            cout << "Error: Invalid adress\n";
-            return -14000;
+        Node* get_next() {
+            return next;
         }
 };
 
-Node::Node(int max_nodes, int new_value) {
-    max_children = 0;
-    number_of_children = 0;
-    value = new_value;
+Node::Node(void) {
+    next = NULL;
+    previous = NULL;
+    content = NULL;
+}
 
-    if(max_nodes > 0) {
-        max_children = max_nodes;
-        children = new Node * [max_nodes];
+class List {
 
-        if(children == NULL) {
-            cout << "ERROR: Can't allocade node's children.\n";
-            exit(EXIT_FAILURE);
+    public:
+        Node *list_head, *list_end;
+
+        List(void);
+
+        void add_node(Node *new_node) {
+            if(list_head == NULL) {
+                list_head = new_node;
+            }
+            else {
+                Node *navigation = list_head;
+
+                while(navigation->get_next()!= NULL) {
+                    navigation = navigation->get_next();
+                }
+                //SEGMENTATION FAULT
+                navigation->add_next(new_node);
+                new_node->add_previous(navigation);
+            }
+            list_end = new_node;
         }
-    }
-    else {
-        cout << "Error: invalid max_children value\n";
-        exit(EXIT_FAILURE);
-    }
+
+        void print() {
+            Node *navigation = list_head;
+            while(navigation != NULL) {
+                cout << (int) navigation->get_content() << endl; // FIXME: Need to find a way to auto cast
+                navigation = navigation->get_next();
+            }
+        }
+};
+
+List::List(void) {
+    list_head = NULL;
+    list_end = NULL;
 }
 
 int main(void) {
-    Node node1(2, 666), node2(2, 333);
 
-    node1.set_node(&node2);
-    cout << node1.get_value() << "\n";
-    cout << node1.get_node_value(0) << "\n";
+    Node node1, node2, node3;
+    List list;
+
+    node1.add_content((int*) 666);
+    node2.add_content((int*) 333);
+    node3.add_content((int*) 111);
+
+    list.add_node(&node1);
+    list.add_node(&node2);
+    list.add_node(&node3);
+    list.print();
 }
